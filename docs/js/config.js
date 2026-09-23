bf3da0f591d12ebf99698a9766fd610b2fb84867
escuelaydeportesevilla-ui <escuelaydeporte.sevilla@gmail.com>
2026-09-23 15:25:15 +0200

    Delete docs/js directory

--- docs/js/config.js
+++
@@ -1,7 +0,0 @@
-// Configuración pública del frontend. No incluir claves secretas aquí (repo público).
-window.ES_CONFIG = {
-  // URL de la Web App de Apps Script (termina en /exec)
-  API_URL: 'PEGAR_AQUI_URL_WEB_APP',
-  // OAuth Client ID (tipo "Aplicación web") de Google Cloud
-  GOOGLE_CLIENT_ID: 'PEGAR_AQUI_CLIENT_ID.apps.googleusercontent.com'
-};
